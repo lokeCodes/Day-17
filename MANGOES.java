c51d447f7d50b041957251caec98af9f3575d47b
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-23 20:36:20 +0530

    Add files via upload

---
+++ MANGOES.java
@@ -0,0 +1,23 @@
+// https://www.codechef.com/problems/MANGOES
+// Question Link^^^
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-- > 0){
+		    int x = s.nextInt();
+		    int y = s.nextInt();
+		    int z = s.nextInt();
+		    int r1 = (z-y)/x;
+		    System.out.println(r1);
+		}
+	}
+}
